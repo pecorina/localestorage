e7c6b78b3a22982448748492116e2542396bc7cc
pecorina <constanza.pecori@gmail.com>
2017-06-16 13:52:45 -0400

    terminado el guiado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-main.js
+//funcion para recuperar los datos q el usuario ingrese
+function guardarDatos(){
+    localStorage.nombre = document.getElementById("nombre").value;
+    localStorage.password = document.getElementById("password").value;
+}
+
+// si los datos ingresados son distintos a undefined, me los imprima en la pantalla.
+//de lo contrario me envie un mensaje "no has indroducido tu nombre y pssword"
+function recuperarDatos(){
+	if((localStorage.nombre != undefined) && (localStorage.password != undefined)) {
+		document.getElementById("datos").innerHTML = "Nombre:" + localStorage.nombre + "<br/> Password: " + localStorage.password;
+	}
+	else{
+		document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
+	}
+}
